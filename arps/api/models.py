f22bcd04b53a466c4b1fe72e11293bbdee10cb44
npoudelp <npoudelp@gmail.com>
2024-06-27 18:24:37 +0545

    model for field created

--- arps/api/models.py
+++ arps/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Fields(models.Model):
+    name = models.CharField(max_length=35, default='Demo Field', blank=False)
+    coordinates = models.TextField(null=False, blank=False)
+    nitrogen = models.FloatField(null=True, blank=True)
+    potassium = models.FloatField(null=True, blank=True)
+    phosphorus = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
