ab0ffe73b11acb26bbf54f4e8437150c3c2d0c8c
npoudelp <npoudelp@gmail.com>
2024-06-27 18:25:17 +0545

    url for field data get and apost created

--- arps/api/urls.py
+++ arps/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .apis import AddField, GetField
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -10,4 +11,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('field/add/', AddField.as_view(), name='add_field'),
+    path('field/get/', GetField.as_view(), name='get_field'),
 ]
