415db8d8d170dc4d9c32f6b4ed884de7c5138129
npoudelp <npoudelp@gmail.com>
2024-06-30 17:00:28 +0545

    api endpoints for get by id, update field added

--- arps/api/apis.py
+++ arps/api/apis.py
@@ -21,6 +21,35 @@ class GetField(APIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request):
-        fields = Fields.objects.all()
+        fields = Fields.objects.all().order_by('-id')
         serializer = FieldsSerializer(fields, many=True)
         return Response(serializer.data, status=200)
+    
+
+# update field data
+class UpdateField(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request, id):
+        try:
+            field = Fields.objects.get(id=id)
+            serializer = FieldsSerializer(instance=field, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=200)
+            return Response(serializer.errors, status=400)
+        except Fields.DoesNotExist:
+            return Response({"error": "Field not found"}, status=404)
+        
+        
+# get field data by id
+class GetFieldById(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, id):
+        try:
+            field = Fields.objects.get(id=id)
+            serializer = FieldsSerializer(field)
+            return Response(serializer.data, status=200)
+        except Fields.DoesNotExist:
+            return Response({"error": "Field not found"}, status=404)
