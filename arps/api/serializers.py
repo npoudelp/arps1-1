99c5aceb667547e23dc8c5ea0f2637756b859768
npoudelp <npoudelp@gmail.com>
2024-06-30 20:21:17 +0545

    serializer for faq added

--- arps/api/serializers.py
+++ arps/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Fields
+from .models import Fields, FrequentQuestions
 
 class FieldsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fields
+        fields = '__all__'
+
+
+class FrequentQuestionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FrequentQuestions
         fields = '__all__'
